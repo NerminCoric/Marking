5c37888d895c49fa9794d30a8fed4636e7c3282f
Nermin Coric <ic22b080@technikum-wien.at>
2022-12-21 19:18:12 +0100

    Add Test

--- src/test/java/a/fhtw/marking/MarkingApplicationTests.java
+++ src/test/java/a/fhtw/marking/MarkingApplicationTests.java
@@ -2,12 +2,23 @@ package a.fhtw.marking;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
 
 @SpringBootTest
-class MarkingApplicationTests {
+
+class UptimeControllerTest {
 
     @Test
-    void contextLoads() {
+    void Marking() {
+        // A - Arrange
+        MarkingController controller = new MarkingController();
+
+        // A - Act
+        var result = controller.mark(60);
+
+        // A - Assert
+        assertEquals(60, result);
     }
 
 }
