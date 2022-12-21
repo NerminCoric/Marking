bdff34ee52445e93a9f480d8f7a7e6db110c565d
Nermin Coric <ic22b080@technikum-wien.at>
2022-12-21 18:43:23 +0100

    initial commit

---
+++ src/test/java/a/fhtw/marking/MarkingApplicationTests.java
@@ -0,0 +1,13 @@
+package a.fhtw.marking;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MarkingApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
